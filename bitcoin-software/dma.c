fddaf8d67f8408bcc5dc457d6341065103399f19
Torbjørn Langland <tl_tl_tl80@hotmail.com>
2015-03-03 21:59:05 +0100

    Added testing for DMA in main.c

--- bitcoin-software/dma.c
+++ bitcoin-software/dma.c
@@ -8,6 +8,8 @@
 
 static volatile uint32_t * module = DMA_BASE;
 
+void dma_reset(void);
+
 void dma_get_load_address0(uint8_t * load);
 void dma_get_store_address0(uint8_t * store);
 void dma_get_request_details0(uint8_t * request);
@@ -20,60 +22,34 @@ void dma_set_load_address0(uint8_t * load);
 void dma_set_store_address0(uint8_t * store);
 void dma_set_request_details0(uint8_t * request);
 
+void dma_reset(void){
+	//EMPTY
+}
 
 // Gets
-void dma_get_load_address0(uint8_t* load){
-	for(int i = 0; i < 4; ++i)
-	{
-		uint32_t value = module[DMA_SLAVE_LREG0];
-		load[0] = (value >> 24) & 0xff;
-		load[1] = (value >> 16) & 0xff;
-		load[2] = (value >>  8) & 0xff;
-		load[3] = (value >>  0) & 0xff;
-	}
+void dma_get_load_address0(uint32_t* load){
+	
+	*load = module[DMA_SLAVE_LREG0];
 }
 
-void dma_get_store_address0(uint8_t* store){
-	for(int i = 0; i < 4; ++i)
-	{
-		uint32_t value = module[DMA_SLAVE_SREG0];
-		store[0] = (value >> 24) & 0xff;
-		store[1] = (value >> 16) & 0xff;
-		store[2] = (value >>  8) & 0xff;
-		store[3] = (value >>  0) & 0xff;
-	}
+void dma_get_store_address0(uint32_t* store){
+	*store = module[DMA_SLAVE_SREG0];
 }
 
-void dma_get_request_details0(uint8_t* request){
-	for(int i = 0; i < 4; ++i)
-	{
-		uint32_t value = module[DMA_SLAVE_RREG0];
-		request[0] = (value >> 24) & 0xff;
-		request[1] = (value >> 16) & 0xff;
-		request[2] = (value >>  8) & 0xff;
-		request[3] = (value >>  0) & 0xff;
-	}
+void dma_get_request_details0(uint32_t* request){
+	*request = module[DMA_SLAVE_RREG0]; 
 }
 
 // Sets
 
-void dma_set_load_address0(uint8_t* load){
-	
-	uint32_t value = 24 << load[0] | 16 << load[1] | 8 << load[2] | 0 << load[3];
-	module[DMA_SLAVE_LREG0] = value;
-	
+void dma_set_load_address0(uint32_t* load){
+	module[DMA_SLAVE_LREG0] = *load;
 }
 
-void dma_set_store_address0(uint8_t* store){
-	
-	uint32_t value = 24 << store[0] | 16 << store[1] | 8 << store[2] | 0 << store[3];
-	module[DMA_SLAVE_SREG0] = value;
-	
+void dma_set_store_address0(uint32_t* store){
+	module[DMA_SLAVE_SREG0] = *store;
 }
 
-void dma_set_request_details0(uint8_t* request){
-	
-	uint32_t value = 24 << request[0] | 16 << request[1] | 8 << request[2] | 0 << request[3];
-	module[DMA_SLAVE_RREG0] = value;
-	
+void dma_set_request_details0(uint32_t* request){
+	module[DMA_SLAVE_RREG0] = *request;
 }
--- bitcoin-software/main.c
+++ bitcoin-software/main.c
@@ -10,6 +10,8 @@
 #include "shmac.h"
 #include "dma.h"
 
+
+
 void main(void)
 {
 	shmac_printf("The Great and Awesome Heterogeneous Bitcoin Miner Project\n\r");
@@ -18,7 +20,9 @@ void main(void)
 	int x, y;
 	shmac_get_tile_loc(&x, &y);
 	shmac_printf("Tile location: (%d, %d)\n\r\n\r", x, y);
-
+	
+	//Hashing module part
+	/*
 	shmac_printf("Resetting SHA256 accelerator... ");
 	sha256_reset();
 	shmac_printf("ok\n\r");
@@ -32,5 +36,82 @@ void main(void)
 
 	shmac_printf("\n\rEnd programme.\n\r");
 	while(1); // Burn cycles until Amber implements something like wfi
+	*/
+	
+	// DMA Part:
+	
+	//shmac_printf("Resetting DMA Module (And I resent the use of ...)");
+	dma_reset();
+	//shmac_printf("ok\n\r");
+	
+	int data_count = 16;
+	uint32_t* load, load_location, load_test, store, store_location, store_test, request, request_test;
+	uint32_t data[data_count];
+	uint32_t data_test[data_count];
+	
+	
+	shmac_printf("Setting Load address: 0x1000 0000 \n");
+	*load = 0x10000000;
+	dma_set_load_address0(load);
+	
+	shmac_printf("Getting Load address:\n");
+	dma_get_load_address0(load_test);
+	shmac_printf("This is the load address gotten: %x\n", *load_test);
+	
+	shmac_printf("Setting Store address: 0x4000 0000 \n");
+	*store = 0x40000000;
+	dma_set_load_address0(load);
+	
+	shmac_printf("Getting Store address:\n");
+	dma_get_store_address0(store_test);
+	shmac_printf("This is the store address gotten: %x\n" *store_test);
+	
+	shmac_printf("Setting up raw data from loading addresses, with following square numbers: 1-4-9-16-25.... (16 in total)\n");
+	
+	uint32_t i;
+	for(i =0; i<data_count; i++){
+		data[i] = (i+1)*(i+1);	
+	}
+	
+	load_location = 0x10000000;
+	uint32_t
+	for(i =0; i<data_count; i++){
+		load_location[i*4] = data[i];	
+	}
+	
+	shmac_printf("Activating DMA: Count: 16 (15 + start), Byte addressing high, ON high. Hex value: 0x00F8 0001\n");
+	*request = 0x00F80001;
+	
+	dma_set_request_details0(request);
+	
+	bool on = true;
+	
+	while(on){
+		
+		dma_get_request_details0(request_test); //TODO: Also test with much larger count if DMA is too fast
+		
+		if(request_test[3] == 0x00){
+			shmac_printf("DMA transfer complete\n");
+			on = false;
+		} else {
+			shmac_printf("DMA active. Sleep for 1000 ms\n");
+			sleep(1);
+		}
+		
+	}
+	
+	store_location = 0x40000000;
+	for(i =0; i<data_count; i++){
+		data_test[i] = store_location[i*4];	
+	}
+	
+	shmac_printf("Now printing out data from old and new location\n");
+	for(i=0, i<data_count; i++){
+		shmac_printf("Old: %d, New: %d\n", data[i], data_test[i]);
+	}
+	
+	shmac_printf("\n\rEnd programme.\n\r");
+	while(1); // Burn cycles until Amber implements something like wfi
+	while (1);
 }
 
