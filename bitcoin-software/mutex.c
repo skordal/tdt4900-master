ce8df170d77be495c90dff9e84d35fbdd55a7bb4
Kristian Klomsten Skordal <kristian.skordal@wafflemail.net>
2015-03-03 18:57:03 +0100

    Add classic (working) mutexes

--- bitcoin-software/mutex.c
+++ bitcoin-software/mutex.c
@@ -6,16 +6,18 @@
 
 #include "mutex.h"
 
+#if !defined(USE_CLASSIC_MUTEXES) && !defined(USE_IMPROVED_MUTEXES)
+#warning "No mutex implementation selected!"
+#endif
+
 void mutex_initialize(mutex_t * mutex)
 {
 	*mutex = MUTEX_INITIALIZER;
 }
 
-// Code below originally from the mutex code in the software/benchmarks/common
-// directory of the SHMAC repository.
-
 void mutex_lock(mutex_t * mutex)
 {
+#ifdef USE_IMPROVED_MUTEXES
 	asm volatile(
 		// Set the initial wait delay:
 		"	mov v3, %[delay]\n"
@@ -23,8 +25,8 @@ void mutex_lock(mutex_t * mutex)
 		// Attempt to lock the mutex:
 		"lock:\n"
 		"	mov v2, #1\n"
-		"	swp v2, v1, [%[mutex]]\n"
-		"	cmp v2, v1\n"
+		"	swp v1, v2, [%[mutex]]\n"
+		"	cmp v1, v2\n"
 		"	bne success\n"
 
 		// If the mutex could not be locked, wait a short amount of
@@ -35,8 +37,8 @@ void mutex_lock(mutex_t * mutex)
 		"	bne delay_loop\n"
 
 		// Increase the delay if the next lock does not succeed:
-		"	adds v3, %[delay]\n"
-		"	movcs v3, #1\n"
+		"	adds v3, v3\n"
+		"	moveq v3, %[delay]\n"
 
 		// Retry:
 		"	b lock\n"
@@ -47,6 +49,43 @@ void mutex_lock(mutex_t * mutex)
 		: [delay] "I" (1)
 		: "v1", "v2", "v3", "memory", "cc"
 	);
+#elif defined(USE_CLASSIC_MUTEXES)
+	__asm__ (
+
+		// save R4
+		"push {r4}\n"
+
+		// init delay between lock attempts
+		"MOV r4, #1\n"
+
+		// try lock and finish if successful
+		"lock:\n"
+		"MOV r2, #1\n"
+		"SWP r1, r2, [r0]\n"
+		"CMP r1, r2\n"
+		"BNE locked\n"
+
+		// load delay and wait in a loop
+		"MOV r3, r4\n"
+		"wait:\n"
+		"SUB r3, #1\n"
+		"CMP r3, #0\n"
+		"BNE wait\n"
+
+		// double delay or load 1 if overflown
+		"ADD r4, r4\n"
+		"CMP r4, #0\n"
+		"MOVEQ r4, #1\n"
+
+		// repeat lock attempt
+		"B lock\n"
+
+		"locked:\n"
+
+		// restore R4
+		"pop {r4}\n"
+	);
+#endif
 }
 
 bool mutex_trylock(mutex_t * mutex)
@@ -66,11 +105,18 @@ bool mutex_trylock(mutex_t * mutex)
 
 void mutex_unlock(mutex_t * mutex)
 {
+#ifdef USE_IMPROVED_MUTEXES
 	asm volatile(
 		"mov v2, #0\n"
 		"swp v1, v2, [%[mutex]]\n"
 		: [mutex] "+r" (mutex)
 		:: "v1", "v2", "memory"
 	);
+#elif defined(USE_CLASSIC_MUTEXES)
+	__asm__ (
+		"MOV r2, #0\n"
+		"SWP r1, r2, [r0]\n"
+	);
+#endif
 }
 
