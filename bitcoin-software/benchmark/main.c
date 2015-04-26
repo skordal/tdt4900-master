11d8a9b8e1784f264aa03093827ab47e1a350e4f
Kristian Klomsten Skordal <kristian.skordal@wafflemail.net>
2015-04-26 13:28:13 +0200

    Move stats to BRAM to avoid caching

--- bitcoin-software/benchmark/main.c
+++ bitcoin-software/benchmark/main.c
@@ -18,9 +18,8 @@
 
 #define BENCHMARK_PASSES	1
 
-static volatile int * stats;
+static volatile int * stats = (volatile void *) 0xf8000060;
 static void benchmark_process(int cpu);
-
 static void print_stats(void * unused);
 
 void main(void)
@@ -28,26 +27,24 @@ void main(void)
 	if(shmac_get_tile_cpu_id() == 0)
 	{
 		// Allocate statistics structure:
-		stats = mm_allocate(sizeof(int) * shmac_get_cpu_count());
+	//	stats = mm_allocate(sizeof(int) * shmac_get_cpu_count());
 		for(int i = 0; i < shmac_get_cpu_count(); ++i)
 			stats[i] = 0;
 
+		shmac_printf("READY %d/%d\n", shmac_get_tile_cpu_id(),
+			shmac_get_cpu_count());
 		timer_start(0, 48828, TIMER_SCALE_1, true, print_stats, NULL);
-
-		shmac_printf("READY %d\n", shmac_get_cpu_count());
 		shmac_set_ready();
-		benchmark_process(shmac_get_tile_cpu_id());
-	} else {
-		shmac_printf("STAT worker %d exited!\n", shmac_get_tile_cpu_id());
 	}
 
-	while(1) asm volatile("nop"); // Burn cycles, use dat power
+	benchmark_process(shmac_get_tile_cpu_id());
+	while(1) asm volatile("nop\n"); // Burn cycles, use dat power
 }
 
 static void benchmark_process(int cpu)
 {
 #if BENCHMARK_PASSES == 1
-	shmac_printf("Hashing started for CPU %d\n", cpu);
+//	shmac_printf("Hashing started for CPU %d\n", cpu);
 
 	struct sha256_context * ctx = sha256_new();
 	uint8_t block[64] = {'a', 'b', 'c'};
@@ -97,8 +94,10 @@ static void print_stats(void * unused)
 		for(int i = 0; i < shmac_get_cpu_count(); ++i)
 		{
 			hps += stats[i];
+			shmac_printf("%d ", stats[i]);
 			stats[i] = 0;
 		}
+		shmac_printf("\n");
 
 		shmac_printf("%d H/s\n", hps);
 		counter = 0;
